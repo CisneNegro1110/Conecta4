669414cd6d8dc3611a087343aa2a038ad93b1e27
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-05-16 06:21:52 -0600

    Initial Commit

---
+++ Shared/Conecta4App.swift
@@ -0,0 +1,17 @@
+//
+//  Conecta4App.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Conecta4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
